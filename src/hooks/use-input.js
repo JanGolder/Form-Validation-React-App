10601d4c9e41fc1b59a91f279f7d74c38fb5777a
Jan Golder <kontakt.jangolder@gmail.com>
2023-08-09 18:49:34 +0200

    add useReducer as a way to manage input states

--- src/hooks/use-input.js
+++ src/hooks/use-input.js
@@ -1,33 +1,53 @@
-import { useState } from "react";
+import {useReducer} from 'react';
 
-const useInput = (validateValue) => {
-  const [enteredValue, setEnteredValue] = useState("");
-  const [isTouched, setIsTouched] = useState(false);
+const initialInputState ={
+    value: '',
+    isTouched: false
+}
 
-  const valueIsValid = validateValue(enteredValue);
-  const hasError = !valueIsValid && isTouched;
+const inputStateReducer=(state, action) =>{
+    if(action.type === 'INPUT'){
+        return {value: action.value, isTouched: state.isTouched}
+    }
+    if(action.type === 'BLUR'){
+        return {value: state.value, isTouched:true}
+    }
+    if(action.type === 'RESET'){
+        return {value: '', isTouched: false}
+    }
+    return initialInputState;
+}
 
-  const valueChangeHandler = (e) => {
-    setEnteredValue(e.target.value);
-  };
 
-  const inputBlurHandler = (e) => {
-    setIsTouched(true);
-  };
 
-  const reset =()=>{
-    setEnteredValue('');
-    setIsTouched(false);
-  };
+const useInput =(valueValidation)=>{
 
-  return {
-    value: enteredValue,
-    isValid: valueIsValid,
+const [inputState, dispatch] = useReducer(inputStateReducer, initialInputState);
+
+const isValid = valueValidation(inputState.value);
+const hasError = !isValid && inputState.isTouched;
+
+const valueChangeHandler=(e)=>{
+    dispatch({type: 'INPUT', value: e.target.value});
+};
+
+const inputBlurHandler = ()=>{
+    dispatch({type: 'BLUR'})
+}
+
+const reset = ()=>{
+    dispatch({type:'RESET'})
+}
+
+return {
+    value: inputState.value,
+    isValid,
     hasError,
     valueChangeHandler,
     inputBlurHandler,
     reset
-  };
+}
+
 };
 
 export default useInput;
